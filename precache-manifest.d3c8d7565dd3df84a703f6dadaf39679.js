97598407b49af646bd183c90d2e0ac494c15c32e
karthikshivkumar <karthik.shivkumar@talentica.com>
2019-02-20 13:25:23 +0530

    Updates

---
+++ precache-manifest.d3c8d7565dd3df84a703f6dadaf39679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "1050aa93bffd5d885394",
+    "url": "/contactmanager/static/js/main.1050aa93.chunk.js"
+  },
+  {
+    "revision": "f6409b910e4be852e627",
+    "url": "/contactmanager/static/js/2.f6409b91.chunk.js"
+  },
+  {
+    "revision": "1050aa93bffd5d885394",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "f6409b910e4be852e627",
+    "url": "/contactmanager/static/css/2.0eb018ad.chunk.css"
+  },
+  {
+    "revision": "1204abec2a31b94188ad72dcdca8cda8",
+    "url": "/contactmanager/index.html"
+  }
+];
